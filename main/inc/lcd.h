ff2e71a2ed1b6f5d75e50f183c2a56813e1d1c49
Rubin Khadka <rubinkhadka84@gmail.com>
2026-07-08 16:43:54 +0200

    ESP32-5 intialisation lcd display files

--- main/inc/lcd.h
+++ main/inc/lcd.h
@@ -8,6 +8,15 @@
 #ifndef MAIN_INC_LCD_H_
 #define MAIN_INC_LCD_H_
 
+#include "esp_err.h"
 
+// PCF8574 I2C Address
+#define LCD_ADDR 		0x27
+
+// PCF8574 Controls
+#define LCD_BACKLIGHT	0x08
+#define LCD_ENABLE		0x04
+#define LCD_RW			0x02
+#define LCD_RS			0x01
 
 #endif /* MAIN_INC_LCD_H_ */
--- main/src/lcd.c
+++ main/src/lcd.c
@@ -5,4 +5,66 @@
  *      Author: Rubin Khadka
  */
 
+#include "lcd.h"
+#include "esp_rom_sys.h"
+#include "freertos/idf_additions.h"
+#include "freertos/projdefs.h"
+#include "i2c.helper.h"
+#include "esp_log.h"
+#include <stdint.h>
+ 
+static const char *TAG = "LCD";
+ 
+// Send nibble
+static void LCD_SendNibble(uint8_t nibble, uint8_t rs)
+{
+	uint8_t data;
+	
+	data = nibble | LCD_BACKLIGHT;	// Backlight always on
+	
+	if (rs)
+	{
+		data |= LCD_RS;		// RS = 1 for data
+	}
+	
+	// Enable High
+	I2C_Write_Reg(LCD_ADDR, data | LCD_ENABLE, 0x00);		// Dummy Write
+	esp_rom_delay_us(1); 	// Small delay
+	
+	// Enable Low
+	I2C_Write_Reg(LCD_ADDR, data & ~LCD_ENABLE, 0x00);
+	esp_rom_delay_us(50);
+}
+ 
+// Send Command
+void LCD_SendCmd(uint8_t cmd)
+{
+	// Send high nibble first
+	LCD_SendNibble(cmd & 0xF0, 0);
+	
+	// Send low nibble
+	LCD_SendNibble((cmd << 4) & 0xF0, 0);
+	
+	// Commands need execution time
+	if (cmd == 0x01 || cmd == 0x02)
+	{
+		vTaskDelay(pdMS_TO_TICKS(20));	// 20 ms
+	} else {
+		vTaskDelay(pdMS_TO_TICKS(1));	// 1 ms
+	}
+}
+
+// Send data
+void LCD_SendData(uint8_t data)
+{
+	// Send high nibble first
+	LCD_SendNibble(data & 0xF0, 1);
+	
+	// Send low nibble
+	LCD_SendNibble((data << 4) & 0xF0, 1);
+	
+	vTaskDelay(pdMS_TO_TICKS(1));
+}
+
+// Initialize LCD
 
